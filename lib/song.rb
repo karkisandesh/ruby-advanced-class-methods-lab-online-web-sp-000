541d8708c622ca1ff0e328bf2788851bdb71baef
Sandesh Karki <karkisandesh@hotmail.com>
2019-02-06 01:07:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,53 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new 
+    song.save
+    return song
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    return song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    self.all.find do |song|
+       song.name == song_name
+    end
+  end
+    
+  def self.find_or_create_by_name(song_name)
+    
+      
+  end
+  
+  def self.alphabetical
+    self.all.sort_by {|song| song}
+  end
+  
+  def self.new_from_filename(str)
+    song = self.new
+      str.split("-").each_with_object([]) do |name, song| 
+        artist_name = name 
+        name = song
+      end
+  end
+
+  def create_from_filename 
+    
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
